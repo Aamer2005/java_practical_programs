470086e95863c4ea83ea92a86871cc3ecbb74adb
Aamer2005 <2023bit064@sggs.ac.in>
2025-01-24 14:47:26 +0530

    Added Pattern 1

--- Practical6.java
+++ Practical6.java
@@ -2,9 +2,8 @@ class Practical6{
 
 	public static void main(String args[])
 	{
-		displayPattern1(7);
-		System.out.println();
-		//displayPattern2(5);
+		displayPattern1(3);
+		displayPattern2(3);
 	}
 
 	static void displayPattern1(int size)
@@ -35,4 +34,40 @@ class Practical6{
 		}
 	}
 	
-
+	static void displayPattern2(int size)
+	{
+		int space;
+		int symbol=0;
+		int i=0;
+		
+		for(int line=1 ; line <= (2*size)-1 ; line++)
+		{
+		
+			if(line>size)
+			{
+				i++;
+			}
+			space = size > line ? size-line:line-size;
+			symbol = size >= line ? 2*line-1 :(size*2-1)-i*2;
+			
+			for( ;space>=1;space--)
+			{
+				System.out.print(" ");
+			}
+			for( ;symbol>=1;symbol--)
+			{
+				
+				if(symbol%2==0 )
+				System.out.print(" ");
+				else if(symbol%2 != 0 && size >= line)
+				System.out.print("+");
+				else if(symbol%2 != 0 && line > size)
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			
+		}
+	}	
+			
+}
