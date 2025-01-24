7397239e664ce194619d3961472050dc7ed1c528
Aamer2005 <2023bit064@sggs.ac.in>
2025-01-24 20:20:23 +0530

    Added Pattern 3

--- Practical6.java
+++ Practical6.java
@@ -2,11 +2,12 @@ class Practical6{
 
 	public static void main(String args[])
 	{
-		displayPattern1(3);
 		displayPattern2(3);
+		displayPattern1(3);
+		displayPattern3(3);
 	}
 
-	static void displayPattern1(int size)
+	static void displayPattern2(int size)
 	{
 		int space;
 		int plus;
@@ -20,7 +21,7 @@ class Practical6{
 				i++;
 			}
 			
-			space = line > size ? line-size: size-line;
+			space = line >= size ? line-size: size-line;
 			plus = line>size?plus=(size*2-1)-i*2:2*line-1;
 			for(;space>=1;space--)
 			{
@@ -34,7 +35,7 @@ class Practical6{
 		}
 	}
 	
-	static void displayPattern2(int size)
+	static void displayPattern1(int size)
 	{
 		int space;
 		int symbol=0;
@@ -67,6 +68,55 @@ class Practical6{
 			
 			System.out.println();
 			
+		}
+	}
+	
+	static void displayPattern3(int size)
+	{
+		int space;
+		int symbol;
+		int i=0;
+		
+		for(int line=1;line<=2*size;line++)
+		{
+			if(line>size)
+			{
+				i++;
+			}
+			
+			space = size>=line?size-line:line-size-1;
+			symbol = size>=line?2*line-1:(size*2+1)-2*i;
+			
+			for( ; space>=1;space--)
+			{
+				System.out.print(" ");
+			}
+			
+			for( ;symbol>=1;symbol--)
+			{
+				if(line%2!=0)
+				{
+					if(symbol%2==0)
+					{
+						System.out.print("+");
+					}
+					else{
+						System.out.print("*");
+					}
+				}
+				else if(line%2==0)
+				{
+					if(symbol%2==0)
+					{
+						System.out.print("*");
+					}
+					else{
+						System.out.print("+");
+					}
+				}
+			}
+			
+			System.out.println();
 		}
 	}	
 			
